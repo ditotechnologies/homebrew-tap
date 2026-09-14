49a51f6c229a71d387187e34e44cf75b330d350a
Anthony Dito <anthony@ditotechnologies.com>
2026-09-14 05:24:18 -0700

    Update Bcue version to 1.4.0

--- Formula/bcue.rb
+++ Formula/bcue.rb
@@ -1,12 +1,12 @@
 class Bcue < Formula
   desc "Command-line tools for BrushCue Script"
   homepage "https://www.brushcue.com"
-  version "1.4.0a3"
+  version "1.4.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/ditotechnologies/brushcue/releases/download/brushcue-v1.4.0a3/bcue-1.4.0a3-macos-aarch64.tar.gz"
-      sha256 "f8d2e94af85f5fc1811efa2885fc21e26e42a0c6e7d1e34f2c9623642ed2913d"
+      url "https://github.com/ditotechnologies/brushcue/releases/download/brushcue-v1.4.0/bcue-1.4.0-macos-aarch64.tar.gz"
+      sha256 "1f0dc697237e0c98903389699abe97c97e4a2788bc449d7aa3fcf28d55618037"
     end
   end
 
